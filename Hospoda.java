0f450829a180b54972d60cfdacb2bc72c8b991b1
Oluwagbemiga13 <daniel.rakovsky@seznam.cz>
2022-04-25 13:21:25 +0200

    Update Hospoda.java

--- Hospoda.java
+++ Hospoda.java
@@ -22,6 +22,8 @@ public class Hospoda {
     static ArrayList<String> soldCocktailNamesList = new ArrayList<String>();
     static ArrayList<Double> soldCocktailPricesList = new ArrayList<Double>();
     int counterOfIndex = 0;
+    public static ArrayList<Integer> indexList = new ArrayList<>();
+    public static ArrayList <String> nameList = new ArrayList<>();
     
     // Hospoda Constructor
     public Hospoda(String name, double balance){
@@ -63,7 +65,7 @@ public class Hospoda {
         } 
         
         else {
-            System.out.println(c.nameOfCustomer + " you are too young for: " + d.nameOfDrink + "!");
+            System.out.println("\n" + c.nameOfCustomer + " you are too young for: " + d.nameOfDrink + "!");
          }
         if(d.amountOfDrink !=0){
         System.out.println("There is : " + d.amountOfDrink + " " + d.nameOfDrink + " left." );}
@@ -81,34 +83,90 @@ public class Hospoda {
      //This method prints important info about Customer  
       public static void closeCustomerAcc(Customer c){
       System.out.println(c.nameOfCustomer + "´s balance : " + c.balanceOfCustomer + "$.");
+      
       }
     
-      //Method searching for ocurance of Customer in ArrayList and saving it´s indexes as Arraylist<Integer>. Not functioning since its hard to implement searching from exact index.
-     /* public static ArrayList<Integer> AnalyseArray(ArrayList<String> a, Customer c) {          
-        int startSearch = 0;
-        ArrayList<Integer> indexOfOcuranceList = new ArrayList<Integer>();
-        
-        int start = a.indexOf(c.nameOfCustomer, startSearch);
-        int indexOfCustomer = -1;
-        
-        while (start != -1){
-        if (start != -1, ){
-        
-        }
-        }
-                
-        indexOfOcuranceList.add(a.indexOf(c.nameOfCustomer));
-        //indexOfOcuranceList.add(a.indexOf((searchStart, c.nameOfCustomer)));
-        return indexOfOcuranceList;
-        }
-      */
       // Prints out Arraylists storing info about transaction
       public static void printArraylists(){
-      System.out.println(customerNamesList);
-      System.out.println(soldCocktailNamesList);
-      System.out.println(soldCocktailPricesList);
+      System.out.println("There were those customers: " + customerNamesList);
+      System.out.println("We sold those drinks: " + soldCocktailNamesList);
+      System.out.println("We charged customers those amounts of money: " + soldCocktailPricesList);
       }
       
+    // method searching for indexes in a where is Customer o located in ArrayList a -> RETURN ArrayList<Integer>
+    public static ArrayList<Integer> searchCustomer (Customer o, ArrayList<String> a){
+        
+        ArrayList<Integer> tempA = new ArrayList<>();
+
+        if(a.indexOf(o.nameOfCustomer) != -1){
+
+            for(int i = 0; i < a.size(); i++){
+                //System.out.println("Index of " + o.nameOfArrayListFinder + " is "  + a.indexOf("Dan"));
+                if(o.nameOfCustomer.equals(a.get(i))){
+                    //System.out.println(a.get(i));
+                    tempA.add(i);
+                   
+                    //indexList.add(tempA.get(i));
+                    //indexList.add(i);
+                }  
+            }
+        }
+        else{
+        System.out.println("There is no " + o.nameOfCustomer + ".");
+        }
+        for (int i = 0 ; i <= tempA.size() -1 ;i++){
+            indexList.add(tempA.get(i)) ;
+        }
+        System.out.println("Size of tempA for " + o.nameOfCustomer + " is : " + tempA.size());
+        return tempA;
+    }
+    
+    // method searching for indexes in a where is Customer o located in ArrayList a -> RETURN ArrayList<Integer>
+    public static ArrayList<Integer> searchDrink (Drink o, ArrayList<String> a){
+        
+        ArrayList<Integer> tempA = new ArrayList<>();
+
+        if(a.indexOf(o.nameOfDrink) != -1){
+
+            for(int i = 0; i < a.size(); i++){
+                //System.out.println("Index of " + o.nameOfArrayListFinder + " is "  + a.indexOf("Dan"));
+                if(o.nameOfDrink.equals(a.get(i))){
+                    tempA.add(i);
+                }  
+            }
+        }
+        else{
+        System.out.println("There is no " + o.nameOfDrink + ".");
+        }
+        for (int i = 0 ; i <= tempA.size() -1 ;i++){
+            indexList.add(tempA.get(i)) ;
+        }
+        System.out.println("Size of tempA for " + o.nameOfDrink + " is : " + tempA.size());
+        return tempA;
+    }
+    
+    // Printing out names stored ArrayList nameList.
+    public static ArrayList<String> printNames(ArrayList<Integer> a){
+        ArrayList<String> tempB = new ArrayList<>();
+        
+        if(a.size() > 0){
+            System.out.println("Printing out all ocurance : ");
+            for(int i = 0; i < a.size(); i++){
+                System.out.print(customerNamesList.get(a.get(i)) + " ");
+                
+        }
+        System.out.println(" \n");
+        }
+        else{
+            System.out.println("There are none names");}
+        return tempB;
+    }
+    
+    //Method counting sold drinks and amount of money earned
+    public static void countDrinks(Drink d){
+        
+    }
+    
       // MAIN METHOD
     public static void main(String[] args) {
     //Creating arrayList of sold products.
@@ -127,26 +185,26 @@ public class Hospoda {
         Drink water = new Drink("Voda", false, 5, 5.0);
         //System.out.println(water.nameOfDrink + " was created.");
         
-    // loop
-       for(int i =1; i<11; i++){
+    // loop for testing
+       for(int i =1; i<3; i++){
             kobyla.sell(dan, beer);
-            //kobyla.sell(evelina, beer);
+            kobyla.sell(evelina, beer);
             kobyla.sell(evelina, water);
             //kobyla.sell(dan, water);
        } 
-       
-       kobyla.sell(dan, beer);
-       kobyla.sell(evelina, water);
        
        closePub(kobyla, water, beer);
        
        printArraylists();
        
        closeCustomerAcc(dan);
+       closeCustomerAcc(evelina);
        
-       dan.bill.add("Pivo");
-
-      // System.out.println(AnalyseArray(customerNamesList, dan));
+      
+      searchCustomer(dan, customerNamesList);
+      System.out.println(indexList);
+      printNames(indexList);
+      searchDrink(beer, soldCocktailNamesList);
     }
 
 }
